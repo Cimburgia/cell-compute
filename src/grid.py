0b581ec1555116946cca4e2ba65ca830c29ec41a
Carina Imburgia <imburgia@cs.washington.edu>
2024-12-11 18:18:35 -0800

    basic random function finished

--- src/grid.py
+++ src/grid.py
@@ -2,13 +2,12 @@ import cell
 import numpy as np
 
 # Cell type dictionary
-cell_type = {
-    'empty':0,
-    'wire':1,
-    'nor':2,
-    'c6_input':3,
-    'c12_input':4
-}
+cell_type = [
+    'empty',
+    'wire',
+    'nor',
+    'c6_input',
+    'c12_input']
 # Grid class
 class Grid:
     """
@@ -22,15 +21,51 @@ class Grid:
     def show_grid(self):
         print(self.grid_layout)   
 
-    def get_neighbors(self, x, y):
-        pass
-
     def add_cell(self, x, y, cell):
-        self.grid_layout[x][y] = cell_type[cell.type]
+        self.grid_layout[x][y] = np.argwhere(cell_type == cell.type)
         self.active_cells[(cell.x, cell.y)] = cell
 
     def reset_plate(self):
         self.grid_layout = np.zeros((self.size, self.size))
+        self.active_cells = {}
+
+    def set_active_cells(self, layout):
+        non_zero_indcies = np.argwhere(layout != 0)
+        coordiantes = [(x,y) for x,y in non_zero_indcies]
+  
+        for x,y in coordiantes:
+            cell_val = layout[x][y]
+            new_cell = cell.Cell(x, y, cell_type[int(cell_val)])
+            self.active_cells[(x,y)] = new_cell
+
+    def get_active_cells(self):
+        return self.active_cells
+    
+    def randomize_plate(self, percent_nor_fill=0.2, percent_wire_fill=0.5,):
+        """
+        This function will randomize the plate with either exclusively nor gates at a
+        certain percentage or both wires and nor gates. This function will be used for
+        running simulations.
+
+        Warning: Running this will reset your plate!
+        """
+        self.reset_plate()
+        total_cells = self.size ** 2
+        num_nor_gates = int(total_cells * percent_nor_fill)
+        num_wires = int(total_cells * percent_wire_fill)
+
+        plate_array = self.grid_layout.flatten()
+        indicies = np.random.choice(total_cells, num_nor_gates + num_wires, replace=False)
+        for i,j in enumerate(indicies):
+            if i <= num_wires:
+                plate_array[j] = 1
+            else:
+                plate_array[j] = 2
+
+        plate_2d = plate_array.reshape(self.size, self.size)
+        self.grid_layout = plate_2d
+        self.set_active_cells(self.grid_layout)
+
 
 
 if __name__ == "__main__":
--- src/gui.py
+++ src/gui.py
@@ -1,123 +1,124 @@
-import tkinter as tk
-from tkinter import ttk
+import sys
+import PyQt6.QtWidgets as QW
+import PyQt6.QtCore as QC
 import cell
 import grid
+from PyQt6.QtWidgets import QSizePolicy
+from layout_colorwidget import Color
 
 colors = {
-    "wire":"green",
-    "nor":"SlateBlue1",
-    "empty":"light yellow"
+    "wire":"lightpink",
+    "nor":"gold",
+    "empty":"powderblue"
 }
 
-class Window:
+class Main_Window(QW.QMainWindow):
     def __init__(self, plate):
-        self.size = plate.size
-        self.window = tk.Tk()
-        self.window.title(f"Cell Compute")
-        self.window.geometry("700x500")
-        # Window Layout
-        self.window.grid_columnconfigure(0, weight=4)
-        self.window.grid_columnconfigure(1, weight=1)
-        self.window.grid_rowconfigure(0, weight=1)
-        # Frames
-        self.grid_frame = tk.Frame(self.window)
-        self.grid_frame.grid(row=0, column=0, sticky="nsew")
-        self.side_frame = tk.Frame(self.window)
-        self.side_frame.grid(row=0, column=1, sticky="nsew", padx=10)
-        # Initialize
-        self.init_plate()
-        self.init_side_frame()
-        self.set_square_window()
-        # Create grid object
+        super().__init__()
         self.plate = plate
+        self.size = plate.size
+        self.setMinimumSize(QC.QSize(700, 500))
+        self.setWindowTitle("Cell Compute")
 
+        # Set central widget
+        central_widget = QW.QWidget()
+        self.setCentralWidget(central_widget)
+
+        # Set high level layout
+        self.base_layout = QW.QHBoxLayout(central_widget)
+        self.plate_layout = QW.QGridLayout()
+        self.side_panel_layout = QW.QVBoxLayout()
+        self.base_layout.addLayout(self.plate_layout, 4)
+        self.base_layout.addLayout(self.side_panel_layout, 1)
+        self.plate_layout.setSpacing(0) 
+
+        # Initialize widgets
+        self.init_plate()
+        self.init_side_panel()
+
+    ## Initialization Methods ##
     def init_plate(self):
         """
-        Initializes an empty plate to place a pattern. Each cell will be blank until updated
+        Initializes an empty plate to place a pattern. Each cell is a button that
+        will be blank until updated.
         """
         self.buttons = []
         for i in range(self.size):
             row = []
             for j in range(self.size):
-                button = tk.Button(self.grid_frame, bg=colors["empty"])
-                button.grid(row=i, column=j, sticky="nsew")
-                button.bind("<Button-1>", lambda event, x=i, y=j: self.cell_on_click(x, y))
+                button = QW.QPushButton()
+                button.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+                button.setStyleSheet("""
+                        QPushButton {
+                            background-color:powderblue;
+                            border: none;
+                            margin: 1px;
+                        }
+                        """)
+                button.clicked.connect(lambda checked, x=i, y=j: self.on_cell_click(x, y))
+                self.plate_layout.addWidget(button, i, j)
                 row.append(button)
             self.buttons.append(row)
+        
+    def init_side_panel(self):
+        button_names = ["Update Plate", "Randomize Plate", "Reset Plate"]
+        button_functions = [self.on_update_click, self.on_randomize_plate_click, self.on_reset_click]
+        # Add three buttons
+        for i in range(3):
+            button = QW.QPushButton(button_names[i])
+            button.clicked.connect(button_functions[i])
+            self.side_panel_layout.addWidget(button)
 
-    def init_side_frame(self):
-        """
-        Initializes the side frame with five buttons and a text area
-        """
-        self.side_frame.grid_columnconfigure(0, weight=1)
-        button_names = ["Update Plate", "Button 2", "Button 3", "Button 4", "Button 5"]
-        button_funtions = [self.update_on_click, self.button2_function, self.button3_function, self.button4_function, self.button5_function]
-        for i, name in enumerate(button_names):
-            button = tk.Button(self.side_frame, text=name, command=button_funtions[i])
-            button.grid(row=i, column=0, pady=5, sticky="ew")
-        # Menu Choice and combobox
-        cell_types = ["empty", "wire", "nor"]
-        self.cell_type_var = tk.StringVar(self.side_frame)
-        self.combobox_cell_type = ttk.Combobox(
-            self.side_frame, 
-            textvariable=self.cell_type_var,
-            values=cell_types,
-            state="readonly"
-        )
-        self.combobox_cell_type.set(cell_types[0])
-        self.side_frame.grid_rowconfigure(len(button_names), weight=1)
-        self.combobox_cell_type.grid(row=len(button_names), column=0, pady=0, sticky="new")
+        # Add dropdown menu
+        self.cell_type_dropdown = QW.QComboBox()
+        self.cell_type_dropdown.addItems(["empty", "wire", "nor"])
+        self.side_panel_layout.addWidget(self.cell_type_dropdown)
 
+        # Add stretch to push widgets to the top
+        self.side_panel_layout.addStretch(1)
     
-    def set_square_window(self):
-        """
-        Used to format the grid frame so that it scales with window size
-        """
-        for i in range(self.size):
-            self.grid_frame.grid_rowconfigure(i, weight=1)
-            self.grid_frame.grid_columnconfigure(i, weight=1)
-
-    def cell_on_click(self, x, y):
-        """
-        Action when button is clicked.
-        """
-        selected_type = self.cell_type_var.get()
-        new_cell = cell.Cell(x, y, type=selected_type)
+    ## Button Click Actions ##
+    def on_cell_click(self, x, y):
+        selected_type = self.cell_type_dropdown.currentText()
+        new_cell = cell.Cell(x, y, selected_type)
         self.plate.add_cell(x, y, new_cell)
-
-        button = self.buttons[x][y]
-        button.config(
-            bg=colors[selected_type]
-        )
-        button.update()
-
-
-
-    def update_on_click(self):
+        self.update_cell_color(new_cell)
+        
+    def on_update_click(self):
         print(self.plate.show_grid())
 
-    def button2_function():
-        print("Button 2 clicked")
-
-    def button3_function():
-        print("Button 3 clicked")
-
-    def button4_function():
-        print("Button 4 clicked")
-
-    def button5_function():
-        print("Button 5 clicked")
-
-    def update_color(self):
-        pass
-
-    def run(self):
+    def on_randomize_plate_click(self):
         """
-        Starts the main application
+        Warning: Running this will reset your plate!
         """
-        self.window.mainloop()
+        self.update_plate_colors(reset=True)
+        self.plate.randomize_plate()
+        self.update_plate_colors()
+
+    def on_reset_click(self):
+        for loc,cell in self.plate.get_active_cells().items():
+            self.update_cell_color(cell, reset=True)
+        self.plate.reset_plate()
+
+    ### Random helper fuctions
+    def update_plate_colors(self, reset=False):
+        for loc,cell in self.plate.get_active_cells().items():
+            self.update_cell_color(cell, reset=reset)
+
+    def update_cell_color(self, cell, reset=False):
+        button = self.buttons[cell.x][cell.y]
+        color = colors["empty"] if reset else colors[cell.type]
+        button.setStyleSheet(f"""
+                    QPushButton {{
+                        background-color: {color};
+                        border: none;
+                        margin: 1px;   
+                    }}
+                """)
 
 if __name__ == "__main__":
-    plate = grid.Grid(15)
-    app = Window(plate)
-    app.run()
+    plate = grid.Grid(20)
+    app = QW.QApplication(sys.argv)
+    window = Main_Window(plate)
+    window.show()
+    sys.exit(app.exec())
